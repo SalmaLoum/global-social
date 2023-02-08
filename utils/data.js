7e555440792e26677cc9e5f7180fa5744b5fc4e2
salmaloum <goforsalma@gmail.com>
2023-02-07 20:46:00 -0800

    removed hw readme

--- utils/data.js
+++ utils/data.js
@@ -1,33 +1,66 @@
 const users = [
-  { username: 'AaranParker', email: 'AranP@gmail.com' },
-  { username: 'AarezNathaniel', email: 'ArazNath@gmail.com' },
-  { username: 'AaronFarish', email: 'AFarish@gmail.com' },
-  { username: 'SarahAarman', email: 'ArmanSarah@gmail.com' },
-  { username: 'AyanTamer', email: 'Ayan_tamer@gmail.com' },
-  { username: 'Mark-Abbas', email: 'Abas_mark@gmail.com' },
-  { username: 'Abdallah', email: 'Abouda@gmail.com' },
-  { username: 'zhi-Zhong', email: 'Zhong_zhi@gmail.com' },
-  { username: 'JaredHekmat', email: 'hekmat.afandy@gmail.com' },
-  { username: 'AstridCashi', email: 'asty-cash@gmail.com' },
-  { username: 'ZohairMurad', email: 'Zozi_mama@gmail.com' },
+  { 
+  "username": "AaranParker",
+   "email": "AranP@gmail.com" 
+  },
+  {
+    "username": "AarezNathaniel",
+    "email": "ArazNath@gmail.com" 
+    },
+  { 
+    "username": "AaronFarish",
+    "email": "AFarish@gmail.com" 
+    },
+  {
+    "username":"SarahAarman", 
+    "email": "ArmanSarah@gmail.com" 
+  },
+  {
+    "username": "AyanTamer",
+    "email": "Ayan_tamer@gmail.com" 
+    },
+  {
+    "username": "Mark-Abbas",
+    "email": "Abas_mark@gmail.com"
+    },
+  {
+    "username":"Abdallah",
+    "email":"Abouda@gmail.com"
+     },
+  { 
+    "username": "zhi-Zhong",
+    "email": "Zhong_zhi@gmail.com"
+   },
+  { 
+    "username": "JaredHekmat",
+    "email":"hekmat.afandy@gmail.com"
+     },
+  { 
+    "username": "AstridCassi",
+    "email": "asty-cash@gmail.com" 
+    },
+  { 
+    "username":"ZohairMurad", 
+    "email": "Zozi_mama@gmail.com"
+   }
 ]
 
 const reactions = [
       {
-        reactionBody: 'Wow!',
-        username: 'AstridCashi',
+        "reactionBody": "Wow!",
+        "username": "AstridCashi"
       },
   {
 
-        reactionBody: 'Indeedo',
-        username: 'AyanTamer',
+        "reactionBody": "Indeedo",
+        "username": "AyanTamer"
     
   },
   {
 
-        reactionBody:
-          'For out in this world we find. Success begins with a fellow will. It is all in the state of mind.',
-        username: 'JaredHekma',
+      "reactionBody":
+          "For out in this world we find. Success begins with a fellow will. It is all in the state of mind.",
+      "username": "JaredHekma"
   }
 ]
 
