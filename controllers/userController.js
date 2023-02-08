f8ea3b608141fae361c6ae6e582dd182cdde7233
salmaloum <goforsalma@gmail.com>
2023-02-07 17:06:29 -0800

    updated config & controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
-const { User, Application } = require('../models');
+const { User, Thought } = require('../models');
 
 module.exports = {
+
   // Get all users
   getUsers(req, res) {
     User.find()
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
+
   // Get a single user
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
@@ -18,21 +20,31 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+
+
   // create a new user
   createUser(req, res) {
     User.create(req.body)
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  // Delete a user and associated apps
+
+  // update user by id
+
+  // Delete a user and associated thoughts
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
       .then((user) =>
         !user
           ? res.status(404).json({ message: 'No user with that ID' })
-          : Application.deleteMany({ _id: { $in: user.applications } })
+          : Thought.deleteMany({ _id: { $in: user.thoughts } })
       )
-      .then(() => res.json({ message: 'User and associated apps deleted!' }))
+      .then(() => res.json({ message: 'User and associated thoughts are deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
 };
+
+
+//post & add a friend
+
+//delete friend
